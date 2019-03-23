d8cf463509260c21ad7771cbe4a618eb6962148e
edujavaonline <edureissantos007@gmail.com>
2019-03-22 21:30:59 -0300

    Alterações 5

--- src/main/resources/db/migration/V02__criar_e_registrar_pessoas.sql
+++ src/main/resources/db/migration/V02__criar_e_registrar_pessoas.sql
@@ -14,8 +14,8 @@ CREATE TABLE pessoa
   CONSTRAINT pk_pessoa PRIMARY KEY (codigo)
 );
 
-INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) VALUES ('Rômulo Göelzer Portolann', 'Rua Osvaldo Minella', '2029', 'Bloco J, Ap 403', 'Cedro', '88341780', 'Camboriú', 'SC', TRUE);
-INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) VALUES ('Natália de Oliveira Vacariano Göelzer', 'Rua Osvaldo Minella', '2029', 'Bloco J, Ap 403', 'Cedro', '88341780', 'Camboriú', 'SC', TRUE);
-INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) VALUES ('Marisa Göelzer', 'Rua Amsterdã', '105', NULL, 'Santa Regina', '88345605', 'Camboriú', 'SC', TRUE);
-INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) VALUES ('Paulo Ricardo Coutinho Gomes', 'Rua Amsterdã', '105', NULL, 'Santa Regina', '88345605', 'Camboriú', 'SC', TRUE);
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) VALUES ('Rômulo Göelzer Portolann', 'Rua Osvaldo Minella', '2029', 'Bloco J, Ap 403', 'Cedro', '88.341-780', 'Camboriú', 'SC', TRUE);
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) VALUES ('Natália de Oliveira Vacariano Göelzer', 'Rua Osvaldo Minella', '2029', 'Bloco J, Ap 403', 'Cedro', '88.341-780', 'Camboriú', 'SC', TRUE);
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) VALUES ('Marisa Göelzer', 'Rua Amsterdã', '105', NULL, 'Santa Regina', '88.345-605', 'Camboriú', 'SC', TRUE);
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) VALUES ('Paulo Ricardo Coutinho Gomes', 'Rua Amsterdã', '105', NULL, 'Santa Regina', '88.345-605', 'Camboriú', 'SC', TRUE);
 INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) VALUES ('João Silva', 'Rua do Abacaxi', '10', NULL, 'Brasil', '38.400-12', 'Uberlândia', 'MG', TRUE);
